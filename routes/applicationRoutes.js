0c7ab2471ed4cca0517b3324867192439b3e5097
Nisma-feather <shanthinifeathers16@gmail.com>
2026-01-30 17:18:48 +0530

    Admission app backend

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post("/", upload.array("files"),createAdmissionApplication);
 router.get("/by-course/:courseAdmissionId",getApplicationsByCourse)
-router.post("/update-status/:applicationId",updateApplicationStatus);
+router.put("/update-status/:applicationId",updateApplicationStatus);
 router.get("/getById/:applicationId",getApplicationById);
 router.put("/update-status/:applicationId",updateApplicationStatus);
 router.get("/details/:applicationId",getApplicationDetails)
